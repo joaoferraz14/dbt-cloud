b2eea5a7a0915fb0b9a2987837b7bccc740c3bc9
User Mac <usermac01@MBP-de-User.Home>
2023-08-28 19:26:43 +0100

    whatever

--- models/marts/jaffle-shop/dim/dim_customers.sql
+++ models/marts/jaffle-shop/dim/dim_customers.sql
@@ -57,3 +57,4 @@ where 1=1
     and dbt_updated_at >= (select DATEADD('day', -3,  max(dbt_updated_at))  from {{this}})
     {% endif %}
 {{ limit_dev_data(date_column_name= 'most_recent_order_date', filter_key_word='and', days_to_filter=640000) }}
+-- whatever
